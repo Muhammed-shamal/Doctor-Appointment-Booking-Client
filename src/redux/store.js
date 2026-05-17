2ece0a95693ec33eb0da3baeaea5361627ab99b3
muhammed.shamal <muhammed.shamal@willsoar.in>
2026-05-17 16:42:33 +0530

    updated schdudle api funcctions

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import dashboardReducer from "../features/dashboard/dashboardSlice";
 import doctorReducer from "../features/doctors/doctorSlice";
-import scheduleReducer from "../features/scheduleSlice";
+import scheduleReducer from "../features/schedules/scheduleSlice";
 
 export const store = configureStore({
   reducer: {
